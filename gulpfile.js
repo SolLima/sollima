94c9c055b158e3e948bc8d27d0a9b3866c61e521
SolLima <solange.lima@sollima.com.br>
2017-04-21 23:17:35 -0300

    Complementos

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+//Task para o sass
+gulp.task('sass', function(){
+    return gulp.src('sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
+
+//Task para o Watch
+gulp.task('watch',function(){
+    gulp.watch('sass/**/*.sass',['sass']);
+});
+
+//Task default Gulp
+gulp.task('default', ['sass', 'watch']);
